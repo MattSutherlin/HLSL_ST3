2ed7e59a79ed3d9e67f2725e6f01ef940b161db7
MattSutherlin <matt@escapeplangames.com>
2019-11-18 22:01:36 -0800

    Actually call load/unload functions in SAP script
    I'm too smart/dumb for my own good.
    
    The plugin_loaded and plugin_unloaded functions only happen at the
    plugin level (duh).  But wait, it gets better... by naming my base
    script file the same as the folder/module it's importing, I've managed
    to confuse the hell out of Sublime and so putting those functions into
    Scripts.py doesn't even work!  So, they go here, which is probably
    better so I can actually do what I'm now doing.
    
    Eventually, I need to make this reload the individual scripts.

--- Scripts/__init__.py
+++ Scripts/__init__.py
@@ -2,3 +2,10 @@ from .IntrinsicHover import IntrinsicHover
 from .OpenIncludedFile import OpenIncludedHlslFileCommand
 from .SwapAttributeParams import SwapAttributeParamsCommand
 from .SwapAttributeParams import SwapAttributeParamsListener
+
+
+def plugin_loaded():
+	SwapAttributeParams.plugin_loaded()
+
+def plugin_unloaded():
+	SwapAttributeParams.plugin_unloaded()
